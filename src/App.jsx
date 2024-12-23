2800be5c837dce3732192eca582ce0566470147b
Amit Yadav <you@example.com>
2024-12-23 22:28:00 +0530

    Live-Cricket-Score

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CricketScore from './Component/CricketScore'
+
+const App = () => {
+  return (
+    <div>
+      <CricketScore/>
+    </div>
+  )
+}
+
+export default App
